9b34295d7e6383888dd17b09b2376ebb62249013
Paulo Victor <pvls2@ic.ufal.br>
2021-08-10 15:17:36 -0300

    add files via upload

---
+++ funcionario.py
@@ -0,0 +1,144 @@
+class Funcionario():
+    nome = ''
+    endereco = ''
+
+    def __init__(self, nome, endereco, tipo_funcionario, salario, sindicato):
+        pass
+        self.nome = nome
+        self.endereco = endereco
+        self.tipo_funcionario = tipo_funcionario #se eh horista, assalariado ou comissario
+        self.salario = salario
+        self.sindicato = sindicato #não sei o que fazer
+
+    def __init__(self, nome):
+        pass
+        self.nome = nome
+
+
+    def adicionarFuncionario(self):
+        self.endereco = input("Insira o endereço: ")
+
+        print("""Selecione o tipo de funcionário:
+        1 - Horista
+        2 - Assalariado
+        3 - Comissionado""")
+        escolha = int(input())
+        while escolha > 3:
+            escolha = int(input("Não entendi, escolha novamente: "))
+        self.tipo_funcionario = escolha
+        self.salario = 0
+        if self.tipo_funcionario == 1:
+            self.salario_por_hora = float(input("Insira o salário por hora trabalhada: "))
+            self.tipo_pagamento = 1
+        elif self.tipo_funcionario == 2:
+            self.salario = float(input("Insira o salário: "))
+            self.tipo_pagamento = 3
+        else:
+            self.taxa_comissao = float(input("Insira a taxa da comissão: "))
+            self.tipo_pagamento = 2
+
+        print("""Faz parte de sindicato:
+        1 - Sim
+        2 - Não""")
+        escolha = int(input())
+        while escolha > 2:
+            escolha = int(input("Não entendi, escolha novamente: "))
+        if escolha == 1:
+            self.sindicato = True
+            self.taxa_sindicato = float(input("Insira a taxa sindical: "))
+        else:
+            self.sindicato = False
+            self.taxa_sindicato = 0
+        
+    def ponto(self):
+        if self.tipo_funcionario == 1:
+            horas = int(input("Insira a quantidade de horas trabalhadas: "))
+            if horas > 8:
+                extra = (horas-8) * self.salario_por_hora * 1.5
+                self.salario += (8 * self.salario_por_hora) + extra
+            else :
+                self.salario += horas * self.salario_por_hora
+            print("Horas cadastradas com sucesso!")
+        else:
+            print("Esse funcionário não pode submeter cartão de ponto")
+            return
+
+    def venda(self):
+        if self.tipo_funcionario == 3:
+            valor = float(input("Insira o valor da venda realizada: "))
+            self.salario += valor * self.taxa_comissao
+            print("Venda cadastradas com sucesso!")
+        else:
+            print("Esse funcionário não pode submeter venda")
+            return
+
+    def servico(self):
+        if self.sindicato:
+            valor = float(input("Insira a taxa do serviço sindical: "))
+            self.salario -= valor
+            print("Serviço sindical cadastrado com sucesso!")
+        else:
+            print("Esse funcionário não pertence ao sindicato")
+            return
+
+    def alterarDados(self):
+        print("\nOpções de alteração:")
+        print("""    1 - Nome
+        2 - Endereço
+        3 - Tipo de funcionário
+        4 - Salário/taxa de comissão
+        5 - Opções do sindicato""")
+        escolha = int(input("O que deseja?\n"))
+        
+        if escolha == 1:
+            self.nome = input("Insira o novo nome: ")
+        
+        elif escolha == 2:
+            self.endereco = input("Insira o novo endereço: ")
+        
+        elif escolha == 3:
+            print("""Selecione o novo tipo de funcionário:
+        1 - Horista
+        2 - Assalariado
+        3 - Comissionado""")
+            escolha = int(input())
+            while escolha > 3:
+                escolha = int(input("Não entendi, escolha novamente: "))
+            self.tipo_funcionario = escolha
+            if self.tipo_funcionario == 1:
+                self.salario_por_hora = float(input("Insira o salário por hora trabalhada: "))
+                self.tipo_pagamento = 1
+            elif self.tipo_funcionario == 2:
+                self.salario = float(input("Insira o salário: "))
+                self.tipo_pagamento = 3
+            else:
+                self.taxa_comissao = float(input("Insira a taxa da comissão: "))
+                self.tipo_pagamento = 2
+        
+        elif escolha == 4:
+            if self.tipo_funcionario == 1:
+                self.salario_por_hora = float(input("Insira o novo salário por hora: "))
+            elif self.tipo_funcionario == 2:
+                self.salario = float(input("Insira o novo salário: "))
+            else:
+                self.taxa_comissao = float(input("Insira a nova taxa da comissão: "))
+        
+        elif escolha == 5:
+            print("\nOpções de sindicato:")
+            print("""    1 - Alterar participação
+        2 - Alterar valor""")
+            escolha = int(input("O que deseja?\n"))
+            if escolha == 1:
+                if self.sindicato == True:
+                    self.sindicato = False
+                    self.taxa_sindicato = 0
+                else:
+                    self.sindicato = True
+                    self.taxa = float(input("Insira a taxa sindical: "))
+            else:
+                if self.sindicato == True:
+                    self.taxa = float(input("Insira a nova taxa sindical: "))
+                else:
+                    print("Esse funcionário não pertence ao sindicato")
+                    return
+        print("Alterações realizadas com sucesso!")
--- main.py
+++ main.py
@@ -1,29 +1,81 @@
-from trabalhador import Trabalhador
+from funcionario import Funcionario
 from folha import Folha
+
+funcionarios = []
+
+def setId():
+    for i in funcionarios:
+        i.id = funcionarios.index(i)
+
+def listar():
+    if funcionarios == []:
+        print("\nAinda não há funcionários cadastrados")
+    else:
+        print("\nid - Nome")
+        for i in funcionarios:
+            print("%2d" %i.id + " - " + i.nome)
 
 def opcoesFuncionario():
     print("\nOpções de funcionário:")
     print("""    1 - Adicionar funcionário
-    2 - Lançamento
-    3 - Remover funcionário
-    4 - Voltar""")
+    2 - Remover funcionário
+    3 - Lançamentos
+    4 - Alterar dados
+    5 - Listar funcionários
+    6 - Voltar""")
     escolha = int(input("O que deseja?\n"))
 
     if escolha == 1:
-        print("vamos add funcionario")
-    elif escolha == 2:
-        print("vamos fazer um lançamento")
-    elif escolha == 3:
-        print("vamos demitir")
-    elif escolha == 4:
+        nome = input("Insira o nome: ")
+        funcionario = Funcionario(nome)
+        funcionario.adicionarFuncionario()
+        funcionarios.append(funcionario)
+        setId()
         opcoes()
+
+    elif escolha == 2:
+        listar()
+        id = int(input("Insira o id do funcionário deseja demitir: "))
+        funcionarios.pop(id)
+        setId()
+        opcoes()
+
+    elif escolha == 3:
+        listar()
+        id = int(input("Insira o id do funcionário que fará o lançamento: "))
+        print("""Insira o tipo de lançamento
+        1 - Cartão de ponto
+        2 - Venda
+        3 - Serviço""")
+        escolha = int(input())
+        if escolha == 1:
+            funcionarios[id].ponto()
+        elif escolha == 2:
+            funcionarios[id].venda()
+        elif escolha == 3:
+            funcionarios[id].servico()
+        opcoes()
+
+    elif escolha == 4:
+        listar()
+        id = int(input("Insira o id do funcionário desejado: "))
+        funcionarios[id].alterarDados()
+        opcoes()
+
+    elif escolha == 5:
+        listar()
+        opcoes()
+
+    elif escolha == 6:
+        opcoes()
+        
     else:
         print("Não entendi, vamos recomeçar o atendimento")
         opcoesFuncionario()
 
 def opcoesFolha():
     print("\nOpções da folha:")
-    print("""    1- Gerar Folha de Pagamento\n
+    print("""    1 - Gerar Folha de Pagamento\n
     2 - Agenda de pagamento\n
     3 - Criar nova agenda de pagamento
     4 - Voltar""")
@@ -31,10 +83,13 @@ def opcoesFolha():
 
     if escolha == 1:
         print("vamos rodar a folha")
+        opcoes()
     elif escolha == 2:
         print("opçoes de agenda")
+        opcoes()
     elif escolha == 3:
         print("criar nova agenda")
+        opcoes()
     elif escolha == 4:
         opcoes()
     else:
@@ -43,9 +98,9 @@ def opcoesFolha():
 
 def opcoes():
     print("\nMenu principal:")
-    print("""    1- Espaço funcionario
-    2- Folha de Pagamento
-    3- Sair""")
+    print("""    1 - Espaço funcionario
+    2 - Folha de Pagamento
+    3 - Sair""")
     escolha = int(input("O que deseja?\n"))
 
     if escolha == 1:
@@ -60,4 +115,19 @@ def opcoes():
         opcoes()
 
 print("Seja bem-vindo ao sistema Folha de Pagamento")
+
+def iniciar():
+    funcionario = Funcionario("primeiro funcionario")
+    funcionarios.append(funcionario)
+    funcionario = Funcionario("segundo funcionario")
+    funcionarios.append(funcionario)
+    funcionario = Funcionario("terceiro funcionario")
+    funcionarios.append(funcionario)
+    funcionario = Funcionario("quarto funcionario")
+    funcionarios.append(funcionario)
+    funcionario = Funcionario("quinto funcionario")
+    funcionarios.append(funcionario)
+    setId()
+
+iniciar()
 opcoes()
